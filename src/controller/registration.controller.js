937c47b8441f2bd8ec84e3024a63b6b5a7cbd155
Ansar Fadillah <53747702+ansxy@users.noreply.github.com>
2023-05-22 05:53:46 +0000

    delete all coments

--- src/controller/registration.controller.js
+++ src/controller/registration.controller.js
@@ -1,28 +1,3 @@
-// const admin = require("firebase-admin")
-// const credentials = require('../../service-account.json')
-
-// admin.initializeApp({
-//     credential : admin.credential.cert(credentials)
-// })
-
-// const signUp = async(req,res) => {
-//     const {email,password} = req.body
-//     try {
-//         const response = await admin.auth().createUser({
-//             email : email,
-//             password : password,
-//             emailVerified : false,
-//             disabled : false
-//         });
-//         res.status(200).json({'status' : 'success' , data : response})
-//     } catch (error) {
-//         res.status(500).json({'status' : 'fail' , message : error})
-//     }
-
-// }
-
-// module.exports = {signUp}
-
 const {createUserWithEmailAndPassword,getAuth} = require('firebase/auth')
 const {app} = require('../utils/firebase.utils')
 
