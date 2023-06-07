388b23bc6083aa4b473f0598e0e4d0f77a19786b
ansxy <ansarfadillah17@gmail.com>
2023-06-08 01:53:32 +0800

    refac : router for get by category

--- src/routes/place.router.js
+++ src/routes/place.router.js
@@ -1,7 +1,9 @@
+const { getByMultipleCategories } = require('../controller/category.controller');
 const { getAllPlaces, getPlaceById } = require('../controller/place.controller');
 const placeRouter = require('express').Router();
 
-placeRouter.get('/', getAllPlaces)
-placeRouter.get('/:id', getPlaceById)
+placeRouter.get('/get-all-place', getAllPlaces)
+placeRouter.get('/get-by-id/:id', getPlaceById)
+placeRouter.get('/get-by-category',getByMultipleCategories)
 
 module.exports = placeRouter
