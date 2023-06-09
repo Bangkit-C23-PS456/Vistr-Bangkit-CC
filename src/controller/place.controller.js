a95c47ef97214071f46c31f9e9e29d8e104dcc47
ansxy <ansarfadillah17@gmail.com>
2023-06-10 00:20:26 +0800

    feat : prep data and input data to database

--- src/controller/place.controller.js
+++ src/controller/place.controller.js
@@ -1,5 +1,6 @@
 const prisma = require('../database/prisma.database')
-
+const filterData = require('../helper/filteredPlaces')
+const places = require('../../src/public/dataPlaced/afterFiltered.json')
 const getAllPlaces = async (req, res) => {
     try {
         const places = await prisma.place.findMany({
@@ -73,5 +74,87 @@ const getPlaceById = async (req, res) => {
     }
 };
 
+const inputDataJson = async(req,res) => {
+    try {
+        const transaction = await prisma.$transaction(
+            places.map(placeData => {
+              const {
+                opening_hours,
+                category,
+                photo,
+                city,
+                ...place
+              } = placeData;
+        
+              const weekRanges = opening_hours?.week_ranges || [];
+              const formattedOpeningHours = weekRanges.flatMap((range, index) => {
+                const day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][index];
+                return range.map(time => ({
+                  day,
+                  openTime: time.open_time,
+                  closeTime: time.close_time,
+                }));
+              });
+              
+              console.log(parseFloat(place.latitude))
+              return prisma.place.create({
+                data: {
+                  place_name: place.place_name,
+                    description: place.description,
+                    activity: place.activity,
+                    min_price: parseInt(place.min_price),
+                    max_price: parseInt(place.max_price),
+                    rating: parseInt(place.rating),
+                    latitude: parseFloat(place.latitude),
+                    longitude: parseFloat(place.longitude),
+                    openingHours: {
+                    createMany: {
+                        data : formattedOpeningHours
+                    },
+                  },
+                  categories: {
+                    create: {
+                      category: {
+                        connectOrCreate: {
+                          where: { name: category },
+                          create: { name: category },
+                        },
+                      },
+                    },
+                  },
+                  photos: {
+                    create: photo
+                      ? [
+                          {
+                            width: photo.images.small.width,
+                            height: photo.images.small.height,
+                            url: photo.images.small.url,
+                          },
+                        ]
+                      : [],
+                  },
+                  city : {
+                    connectOrCreate : {
+                        where : {name : city},
+                        create : {name : city}
+                    } 
+                  }
+                },
+              });
+            })
+          );
+          return res.status(200).json({status : 'success',data :transaction})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({status:'fail',message : error})
+    }
+}
 
-module.exports = { getAllPlaces, getPlaceById }
+const prepData = async(req,res) => {
+    const data = await filterData()
+
+    res.status(200).send(data)
+}
+
+
+module.exports = { getAllPlaces, getPlaceById,inputDataJson,prepData }
--- src/routes/place.router.js
+++ src/routes/place.router.js
@@ -1,9 +1,11 @@
 const { getByMultipleCategories } = require('../controller/category.controller');
-const { getAllPlaces, getPlaceById } = require('../controller/place.controller');
+const { getAllPlaces, getPlaceById, inputDataJson, prepData } = require('../controller/place.controller');
 const placeRouter = require('express').Router();
 
 placeRouter.get('/get-all-place', getAllPlaces)
 placeRouter.get('/get-by-id/:id', getPlaceById)
 placeRouter.get('/get-by-category',getByMultipleCategories)
+placeRouter.get('/input-data',inputDataJson)
+placeRouter.get('/prep-data',prepData)
 
 module.exports = placeRouter
