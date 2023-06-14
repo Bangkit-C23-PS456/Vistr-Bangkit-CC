283a471ec02c444a3b5222935b44024af00c7622
ansxy <ansarfadillah17@gmail.com>
2023-06-14 23:10:26 +0800

    feat : itinary

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -9,9 +9,27 @@ const userPreference = async(req,res) => {
         }
         return res.status(200).json({status:'success',data : data.data.preferences})
     } catch (error) {
+        return res.status(500)    
+    }
+}
+
+const userIternary = async(req,res) => {
+    const {latitude, longtitude} = req.query
+    try {
+        const iternary = await axios.get(`https://vistr-ml-ouf6j3ewba-et.a.run.app/user/itinerary?latitude=${latitude}&longtitude=${longtitude}`)
+        const data = [
+            iternary.data.prefrences.todo_recommendation,
+            iternary.data.prefrences.hotel_recommendation,
+            iternary.data.prefrences.restaurant_recommendation 
+        ]
+        if(data[0] === 0) {
+            return res.status(204).json({status:'fail',message : "Tidak ada yang cocok"})
+        }
+        return res.status(200).json({status:'success',data : data})
+    } catch (error) {
         console.log(error)
         return res.status(500)    
     }
 }
 
-module.exports= userPreference
+module.exports= {userPreference,userIternary}
