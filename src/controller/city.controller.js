cf4a3e7e95318360edbd668e8efb7c7a4fec1dca
Gabriel D Lawrence Jamrewav <gabriel.jamrewav@gmail.com>
2023-06-15 23:04:38 +0800

    fix: get place by city response

--- src/controller/city.controller.js
+++ src/controller/city.controller.js
@@ -1,36 +1,48 @@
 const prisma = require("../database/prisma.database")
 
-const getAllCities = async (req, res) =>{
+const getAllCities = async (req, res) => {
     try {
         const cities = await prisma.city.findMany()
-        return res.status(200).json({status: "success", data : cities})  
+        return res.status(200).json({ status: "success", data: cities })
     } catch (error) {
-        return res.status(500).json({status: "fail", data : error})  
+        return res.status(500).json({ status: "fail", data: error })
     }
 }
 
-const getAllPlaceInCity = async (req,res) => {
-    const {city} = req.params
-    if(city !== null) {
+const getAllPlaceInCity = async (req, res) => {
+    const { city } = req.params
+    if (city !== null) {
         try {
             const places = await prisma.city.findFirstOrThrow({
-                where : {
-                    name : city
+                where: {
+                    name: city
                 },
-                include : {
-                    places : true
+                include: {
+                    places: {
+                        include: {
+                            photos: true,
+                            openingHours: true,
+                            city: true,
+                            categories: {
+                                include: {
+                                    category: true,
+                                },
+                            },
+                        },
+                    }
                 }
-            })  
-            return res.status(200).json({status : "success" , data: places})
+
+            })
+            return res.status(200).json({ status: "success", data: places })
         } catch (error) {
-            if(error.name === "NotFoundError") {
-                return res.status(404).json({status: 'fail', message : "Kota tidak di temukan"})
+            if (error.name === "NotFoundError") {
+                return res.status(404).json({ status: 'fail', message: "Kota tidak di temukan" })
             }
-            return res.status(200).json({status : "fail", data : error})
+            return res.status(200).json({ status: "fail", data: error })
         }
     } else {
-        return res.status(404).json({status: 'fail', message : "Kota tidak di temukan"})
+        return res.status(404).json({ status: 'fail', message: "Kota tidak di temukan" })
     }
 }
 
-module.exports = {getAllCities,getAllPlaceInCity}
+module.exports = { getAllCities, getAllPlaceInCity }
